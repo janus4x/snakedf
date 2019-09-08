6e3336d767aec77ad2fece4682904c08331e2112
janus4x <max33303@rambler.ru>
2019-09-08 22:35:42 +0300

    test
    test pust pls ignore

---
+++ frmSnake.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmSnake
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrGame = New System.Windows.Forms.Timer(Me.components)
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.picGame = New System.Windows.Forms.PictureBox()
+        Me.toppanel = New System.Windows.Forms.Panel()
+        Me.lblscoreText = New System.Windows.Forms.Label()
+        Me.lblscore = New System.Windows.Forms.Label()
+        CType(Me.picGame, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.toppanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tmrGame
+        '
+        '
+        'lblMessage
+        '
+        Me.lblMessage.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblMessage.BackColor = System.Drawing.Color.Black
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblMessage.ForeColor = System.Drawing.Color.DarkOrange
+        Me.lblMessage.Location = New System.Drawing.Point(38, 518)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(684, 56)
+        Me.lblMessage.TabIndex = 8
+        Me.lblMessage.Text = "Нажми Enter для страта"
+        Me.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'picGame
+        '
+        Me.picGame.BackColor = System.Drawing.Color.Black
+        Me.picGame.Location = New System.Drawing.Point(0, 336)
+        Me.picGame.Name = "picGame"
+        Me.picGame.Size = New System.Drawing.Size(768, 768)
+        Me.picGame.TabIndex = 7
+        Me.picGame.TabStop = False
+        '
+        'toppanel
+        '
+        Me.toppanel.BackColor = System.Drawing.Color.Black
+        Me.toppanel.Controls.Add(Me.lblscoreText)
+        Me.toppanel.Controls.Add(Me.lblscore)
+        Me.toppanel.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.toppanel.Location = New System.Drawing.Point(0, 0)
+        Me.toppanel.Name = "toppanel"
+        Me.toppanel.Size = New System.Drawing.Size(591, 46)
+        Me.toppanel.TabIndex = 9
+        '
+        'lblscoreText
+        '
+        Me.lblscoreText.Font = New System.Drawing.Font("a_BosaNova", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblscoreText.ForeColor = System.Drawing.Color.White
+        Me.lblscoreText.Location = New System.Drawing.Point(13, 3)
+        Me.lblscoreText.Name = "lblscoreText"
+        Me.lblscoreText.Size = New System.Drawing.Size(76, 41)
+        Me.lblscoreText.TabIndex = 1
+        Me.lblscoreText.Text = "Очки:"
+        Me.lblscoreText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblscore
+        '
+        Me.lblscore.Font = New System.Drawing.Font("a_BosaNova", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblscore.ForeColor = System.Drawing.Color.White
+        Me.lblscore.Location = New System.Drawing.Point(81, 0)
+        Me.lblscore.Name = "lblscore"
+        Me.lblscore.Size = New System.Drawing.Size(66, 41)
+        Me.lblscore.TabIndex = 0
+        Me.lblscore.Text = "0"
+        Me.lblscore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmSnake
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.HTG_Snake.My.Resources.Resources.фон
+        Me.ClientSize = New System.Drawing.Size(768, 1100)
+        Me.Controls.Add(Me.toppanel)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.picGame)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmSnake"
+        Me.Text = "HTG_Snake"
+        CType(Me.picGame, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.toppanel.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents tmrGame As System.Windows.Forms.Timer
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+    Friend WithEvents picGame As System.Windows.Forms.PictureBox
+    Friend WithEvents toppanel As Panel
+    Friend WithEvents lblscoreText As Label
+    Friend WithEvents lblscore As Label
+End Class
---
+++ frmSnake.vb
@@ -0,0 +1,312 @@
+﻿Imports System.IO
+Imports System.Runtime.Serialization.Formatters.Binary
+
+Public Class frmSnake
+    Public u, d, l, r As Boolean
+    <Serializable>
+    Structure Score_table
+        Dim Score As Integer
+        Dim Name As String
+        Dim Phone As String
+    End Structure
+
+    Private Const intGrow As Integer = 3
+    Private Const intWidth As Integer = 15
+
+    Private cSnake As clsSnake
+    Private cMovement As clsMovement
+
+    Private blnMoving As Boolean = False
+    Private blnExpanding As Boolean = False
+
+    Private rectFood As Rectangle
+
+    Private intScore As Integer
+    Private Declare Function sndPlaySound Lib "winmm.dll" Alias "sndPlaySoundA" _
+(ByVal lpszName As String, ByVal dwFlags As Long) As Long
+    Const SND_SYNC = &H0
+
+    Public Sub Feed()
+
+        Dim pntFood As Point
+
+        Do
+
+            pntFood = Randomize()
+
+            If Not (cSnake Is Nothing) Then
+
+                If Not cSnake.FoodPlacedOnSnake(pntFood) Then Exit Do
+
+            Else
+
+                Exit Do
+
+            End If
+
+        Loop
+
+        rectFood.Location = pntFood
+
+    End Sub
+
+    Private Sub Die()
+        Dim arr(10) As Score_table
+        arr(0).Name = "Max" : arr(0).Score = intScore : arr(0).Phone = "test"
+        Using fstream As FileStream = File.Create("test.dat")
+            Dim bf As New BinaryFormatter
+            bf.Serialize(fstream, arr)
+        End Using
+        frmEndGame.Show()
+        tmrGame.Stop()
+
+    End Sub
+
+
+    Private Sub Initialize()
+
+        intScore = 0
+
+        rectFood = New Rectangle(0, 0, intWidth, intWidth)
+
+        Feed()
+
+        Dim pntStart As New Point(CInt(picGame.ClientSize.Width / 2 / intWidth + 0.5) * intWidth, CInt(picGame.ClientSize.Height / 2 / intWidth + 0.5) * intWidth)
+
+        cSnake = New clsSnake(pntStart, intWidth, 1)
+
+        cMovement = New clsMovement(intWidth, cSnake.Head.Loc, clsMovement.intDirection.Right)
+
+        blnExpanding = True
+
+    End Sub
+
+    Private Sub UpdateUI()
+
+        Static iGrow As Integer = intGrow
+
+        Static intAddSeg As Integer
+
+        If Not blnMoving Then Exit Sub
+
+        cMovement.Move(picGame.ClientRectangle)
+
+
+        If cSnake.FoodPlacedOnSnake(cMovement.Location) Then
+
+            iGrow = 0
+            intAddSeg = 0
+
+            Die()
+
+            Return
+
+
+        ElseIf rectFood.Contains(cMovement.Location) Then
+
+            iGrow += intGrow
+
+            blnExpanding = True
+
+            Feed()
+
+            intScore += 5
+
+            Text = intScore.ToString
+            lblscore.Text = Text
+
+
+        End If
+
+        If blnExpanding Then
+
+            If iGrow < intGrow Then iGrow = intGrow
+
+            If intAddSeg >= iGrow Then
+
+                blnExpanding = False
+
+                intAddSeg = 0
+                iGrow = 0
+
+                cSnake.Move(cMovement.Location)
+
+            Else
+
+                cSnake.Eat(cMovement.Location)
+
+                intAddSeg += 1
+
+            End If
+
+        Else
+
+            cSnake.Move(cMovement.Location)
+
+        End If
+
+    End Sub
+
+    Private Sub DisplayMessage(ByVal strMsg As String)
+
+        lblMessage.Text = strMsg
+        lblMessage.Visible = True
+        blnMoving = False
+
+        tmrGame.Enabled = False
+
+    End Sub
+
+    Public Function Randomize() As Point
+
+        Dim rnd As New Random(Now.Second)
+
+        Dim intScreenWidth As Integer = ((ClientRectangle.Width \ intWidth) - 2) * intWidth
+        Dim intScreenHeight As Integer = ((ClientRectangle.Height \ intWidth) - 2) * intWidth
+
+        Dim intX As Integer = rnd.Next(0, intScreenWidth)
+        Dim intY As Integer = rnd.Next(0, intScreenHeight)
+
+        intX = (intX \ intWidth) * intWidth
+        intY = (intY \ intWidth) * intWidth
+
+        Return New Point(intX, intY)
+
+    End Function
+
+    Private Sub HideMessage()
+
+        Me.Text = "Score: " + intScore.ToString
+
+        lblMessage.Visible = False
+        blnMoving = True
+        tmrGame.Enabled = True
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        r = True
+        Initialize()
+        Me.FormBorderStyle = FormBorderStyle.None
+
+
+    End Sub
+
+    Private Sub picGame_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles picGame.Paint
+
+        If Not blnMoving Then
+
+            e.Graphics.Clear(picGame.BackColor)
+
+            Exit Sub
+
+        End If
+
+        e.Graphics.FillEllipse(Brushes.DarkOrange, rectFood)
+
+        Dim segCurrent As clsSegment
+
+
+        For Each segCurrent In cSnake.NumberOfSegments
+
+            e.Graphics.FillRectangle(Brushes.DarkOrange, segCurrent.Rect)
+
+        Next
+
+    End Sub
+
+    Private Sub tmrGame_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrGame.Tick
+
+        UpdateUI()
+        picGame.Invalidate()
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblscoreText.Click
+
+    End Sub
+
+    Private Sub lblMessage_Click(sender As Object, e As EventArgs) Handles lblMessage.Click
+
+    End Sub
+
+    Private Sub Form1_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
+
+        Select Case e.KeyCode
+
+            Case Keys.Enter
+
+                HideMessage()
+
+            Case Keys.Escape
+
+                If blnMoving Then
+
+                    DisplayMessage("Press Enter to continue or Escape to quit.")
+
+                Else
+
+                    Me.Close()
+
+                End If
+
+        End Select
+
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+
+        Select Case e.KeyCode
+
+            Case Keys.Right
+
+                If l = True Then
+                    Exit Sub
+                Else
+                    cMovement.Direction = clsMovement.intDirection.Right
+                    r = True
+                    u = False
+                    d = False
+                    l = False
+                End If
+            Case Keys.Down
+                If u = True Then
+                    Exit Sub
+                Else
+                    cMovement.Direction = clsMovement.intDirection.Down
+                    d = True
+                    u = False
+                    r = False
+                    l = False
+                End If
+            Case Keys.Left
+                If r = True Then
+                    Exit Sub
+                Else
+                    cMovement.Direction = clsMovement.intDirection.Left
+                    l = True
+                    r = False
+                    d = False
+                    u = False
+                End If
+            Case Keys.Up
+
+                If d = True Then
+                    Exit Sub
+                Else
+                    cMovement.Direction = clsMovement.intDirection.Up
+                    u = True
+                    r = False
+                    d = False
+                    l = False
+                End If
+        End Select
+
+
+    End Sub
+
+    Private Sub PicGame_Click(sender As Object, e As EventArgs) Handles picGame.Click
+
+    End Sub
+End Class
